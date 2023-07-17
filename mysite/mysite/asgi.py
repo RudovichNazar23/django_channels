620270347ccdfce049af1959877bf69f5b6fb250
Nazar <you@example.com>
2023-07-17 16:44:53 +0200

    wrote new consumer and added channel_layer

--- mysite/mysite/asgi.py
+++ mysite/mysite/asgi.py
@@ -1,14 +1,6 @@
-"""
-ASGI config for mysite project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
 import os
-import chat.routing
 
+import chat.routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -18,7 +10,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
-
 
 application = ProtocolTypeRouter(
     {
